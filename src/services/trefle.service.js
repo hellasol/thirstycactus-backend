714f87ade021aa90350ee86d93cadc55bc6a7086
Rafael Kallis <rk@rafaelkallis.com>
2020-08-03 17:52:00 +0200

    fix(trefle): fixed trefle api

--- src/services/trefle.service.js
+++ src/services/trefle.service.js
@@ -8,19 +8,15 @@ module.exports = {
    * @returns {String[]}
    */
   async getPlantsByName(query) {
-    const response = await fetch(
-      `https://trefle.io/api/species?token=${config.TREFLE_TOKEN}&q=${query}&page_size=100`
-    );
-    const plants = await response.json();
-
+    const response = await fetch(`https://trefle.io/api/v1/species?token=${config.TREFLE_TOKEN}&q=${query}&page_size=100`);
+    const { data: plants } = await response.json();
     return Promise.all(plants.map((plant) => this.getPlant(plant.id)));
   },
 
   async getPlant(id) {
-    const response = await fetch(
-      `https://trefle.io/api/species/${id}?token=${config.TREFLE_TOKEN}`
-    );
-    return response.json();
+    const response = await fetch(`https://trefle.io/api/v1/species/${id}?token=${config.TREFLE_TOKEN}`);
+    const { data } = await response.json();
+    return data;
   },
 
   async enrichPlant(plant) {
@@ -32,6 +28,5 @@ module.exports = {
     plant.scientificName = trefleData.scientific_name;
     plant.familyName = trefleData.family_common_name;
     plant.growth = trefleData.growth;
-    console.log(plant);
   },
 };
